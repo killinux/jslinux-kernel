b104d1fc26a12d93c82dbbbd03ebe49ceb35cd86
killinux <killinux@163.com>
2020-05-29 19:54:28 +0800

    taptun patch about linux-2.6.20/.config

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+tar xvf linux-2.6.20.tar.gz
 cd linux-2.6.20
 cp ../src/config_linux-2.6.20 .config
 patch -p1 < ../src/patch_linux-2.6.20
 patch -p1 <../patch/2.6.20_common_fixes.patch
 #diff -up Makefile ../Makefile >../kernel2.6_makefile.patch
 patch -p1 <../patch/kernel2.6_makefile.patch
+patch -p1 <../patch/taptun_config.patch
 
 cd ../src
 make 
